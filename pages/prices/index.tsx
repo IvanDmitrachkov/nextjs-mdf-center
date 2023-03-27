8cd62736ce52cd3ef20fccc58ac0ac751a95e893
dmitrachkovivan <dmitrachkovivan@yandex.ru>
2023-03-27 14:01:45 +0300

    добавил кнопку скачать файлом

--- pages/prices/index.tsx
+++ pages/prices/index.tsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import css from 'pages/prices/pagePrices.module.css'
 import cn from 'classnames'
 import PriceGroup from './PriceGroup'
+import { IconDownload } from './IconDownload'
 
 const PagePrices = () => {
   const { data: banner } = useQuery(
@@ -30,28 +31,38 @@ const PagePrices = () => {
     })
 
   return (
-    <div>
-      <Banner {...banner} />
-      <div className={cn('wrapper py-64 sm:overflow-auto', css.container)}>
-        {prices?.map(({ title, categories }, key) => (
-          <div key={key} className='w-2/3 md:w-full sm:w-full'>
-            <h2 className='text-32 mt-64 mb-32 font-medium'>{title}</h2>
-            {
-                categories?.map(({ products, title: catTitle }, catKey) => (
-                <PriceGroup title={catTitle} products={products} key={catKey} />
-                ))
-            }
-          </div>
-        ))}
-      </div>
-      <ul className='wrapper list pb-64'>
-        {pricesInfo?.advantages?.map((item, key) => (
-          <li key={key} className='text-24 mb-24'>
-            {item}
-          </li>
-        ))}
-      </ul>
-    </div>
+        <div>
+            <Banner {...banner} />
+            <div className={cn('wrapper py-64 sm:overflow-auto', css.container)}>
+                <div className='flex justify-between relative sm:flex-col-reverse'>
+                    <div className='w-2/3 md:w-full sm:w-full'>
+                        {prices?.map(({ title, categories }, key) => (
+                            <div key={key}>
+                                <h2 className='text-32 mt-64 mb-32 font-medium'>{title}</h2>
+                                {
+                                    categories?.map(({ products, title: catTitle }, catKey) => (
+                                        <PriceGroup title={catTitle} products={products} key={catKey}/>
+                                    ))
+                                }
+                            </div>
+                        ))}
+                    </div>
+                    <div className='mt-64 w-1/3 pl-80 md:pl-40 sm:w-full sm:pl-0'>
+                        <a className='text-18 flex items-center sticky top-100 hover:text-gray' href='/files/prays-tekhnichka.docx'>
+                            <IconDownload />
+                            Скачать файлом
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <ul className='wrapper list pb-64'>
+                {pricesInfo?.advantages?.map((item, key) => (
+                    <li key={key} className='text-24 mb-24'>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </div>
   )
 }
 export default PagePrices
